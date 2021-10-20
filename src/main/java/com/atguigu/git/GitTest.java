b4f38a3cf023a9223830d299b3c86972ad2091e6
lzg <1525822994@qq.com>
2021-10-20 13:21:24 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author lzg
+ * @create 2021-10-20 下午 1:15
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git！");
+    }
+}
